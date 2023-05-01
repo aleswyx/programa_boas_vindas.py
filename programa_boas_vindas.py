66e1499c1c929c2a9e596de3dcf837adc2767968
Alessandro França Ramos <111668066+aleswyx@users.noreply.github.com>
2023-05-01 15:16:56 -0300

    adicionei arquivos via upload

---
+++ programa_boas_vindas.py
@@ -0,0 +1,4 @@
+# Este programa pede que o usuário insira seu nome e, em seguida, imprime uma mensagem de boas-vindas com o nome do usuário.
+
+nome = input("Qual é o seu nome? ")
+print("Olá, " + nome + "! Bem-vindo ao meu programa em Python!")
